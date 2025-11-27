474bbe0e24ccfdd2681e873a409a70953ae56e97
Pete LoGiudice <pete.logiudice@zohomail.com>
2025-11-26 22:09:31 -0600

    Front end pre-work

--- landing/urls.py
+++ landing/urls.py
@@ -6,5 +6,8 @@ app_name = "landing"
 
 urlpatterns = [
     path("", views.index, name="landing_page"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("tradition/", views.tradition, name="tradition"),
     path("figma/", views.landing_view, name="figma_landing"),
 ]
--- landing/views.py
+++ landing/views.py
@@ -5,6 +5,22 @@ def index(request):
     return render(request, "landing/index.html")
 
 
+def about(request):
+    return render(request, "landing/about.html")
+
+
+def contact(request):
+    return render(request, "landing/contact.html")
+
+
+def gallery(request):
+    return render(request, "landing/gallery.html")
+
+
+def tradition(request):
+    return render(request, "landing/tradition.html")
+
+
 def landing_view(request):
     """
     Renders the main landing page template.
